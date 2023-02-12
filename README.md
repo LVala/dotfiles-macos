7d6602f195827a124dd8821dc93565260042c9a7
Łukasz Wala <lukasz.wala@swmansion.com>
2023-02-12 18:53:25 +0100

    Bugfix in linking script

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Dotfiles for MacOS Ventura (M1)
 
-To install the configuration, use script below
+Work in progress.
+
+To install the configuration, use script below.
 
 ```bash
 curl -fsSL https://raw.githubusercontent.com/LVala/dotfiles-macos/main/scripts/install.sh | sh
--- scripts/link_config.sh
+++ scripts/link_config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -s ../config/alacritty $HOME/.config/
-ln -s ../config/fish $HOME/.config/
-ln -s ../config/skhd $HOME/.config/
-ln -s ../config/yabai $HOME/.config/
+ln -s $HOME/dotfiles/config/alacritty $HOME/.config/
+ln -s $HOME/dotfiles/config/fish $HOME/.config/
+ln -s $HOME/dotfiles/config/skhd $HOME/.config/
+ln -s $HOME/dotfiles/config/yabai $HOME/.config/
 
